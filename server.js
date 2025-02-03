f13f1ff681bae4194900845f2de31352f60cee44
yoke raj <yokeraj285@example.com>
2025-02-03 10:28:00 +0530

    changes modified

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ const client = new MongoClient(uri);
 let database;
 let collection;
 
+console.log("Git hub Connected"); 
+
 async function connectDB() {
     try {
         await client.connect();
